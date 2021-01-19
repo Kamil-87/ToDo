67de06d12e9c7340c8272e772e0ad4497e6650f0
Kamil-87 <k.kamil87-87@yandex.ru>
2021-01-19 10:07:39 +0500

    Start project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MEVN + Vuetify
+
+Подключаем нужные пакеты
+* vue create frontend
+* vue add vuetify
+* npm install material-design-icons-iconfont -D
+* npm install @fortawesome/fontawesome-free -D
+* npm install axios
+
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+        app
+        color="green"
+        dark
+    >
+      <v-app-bar-nav-icon @click.stop="mini = !mini"/>
+      <v-toolbar-title
+          style="width: 300px"
+          class="ml-0 pl-4"
+      >
+        <span class="hidden-sm-and-down">ToDo List</span>
+      </v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+
+    </v-main>
+    <v-btn
+        v-scroll="onScroll"
+        bottomcolor="red"
+        dark
+        fab
+        fixedright
+        @click="toTop"
+    >
+      <v-icon>mdi-chevron-up</v-icon>
+    </v-btn>
+  </v-app>
+</template>
+<script>
+export default {
+  props: {
+    source: {
+      type: String
+    }
+  },
+  data: () => ({
+    mini: false
+  }),
+  methods: {
+    onScroll(e) {
+      if (typeof window === 'undefined') return
+      const top = window.pageYOffset || e.target.scrollTop || 0
+      this.fab = top > 20
+    },
+    toTop() {
+      this.$vuetify.goTo(0)
+    }
+  }
+}
+</script>
